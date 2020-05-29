42f439a130b7798ce2ffa479193d6b569475c32d
Sasha Matijasic <sasha@selectnull.com>
2020-05-29 09:34:58 +0200

    Add utils script

---
+++ utils.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+is_hg() {
+    is_repository "$1" hg
+}
+
+is_git() {
+    is_repository "$1" git
+}
+
+is_repository() {
+    directory=$1
+    scm=$2
+    test -d "$directory/.$scm"
+}
